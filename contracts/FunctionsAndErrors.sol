6882941b04120d11fde3d96863d95bde8d03f893
okste1234 <okste1234@gmail.com>
2024-03-25 00:45:17 +0100

    CHORE: set state variables for test

--- contracts/FunctionsAndErrors.sol
+++ contracts/FunctionsAndErrors.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract FunctionsandErrors {
-    uint256 public maxVal;
-    uint256 three;
+contract FunctionsAndErrors {
+    uint256 public maxVal = 6;
+    uint256 public three = 3;
 
     function requireError(uint256 _val) public view {
-        require(maxVal >= _val, "You can't pass a greater than than maxVal");
+        require(
+            maxVal >= _val,
+            "You can't pass a value greater than than maxVal"
+        );
     }
 
     function assertError(uint256 _val) public view {
